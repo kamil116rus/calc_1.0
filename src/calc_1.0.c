2d10be779f9725ab8e7f32a9510bcdb3163c1fb4
kamil116rus <kh8@rambler.ru>
2023-04-11 14:50:34 +0300

    stack

--- src/calc_1.0.c
+++ src/calc_1.0.c
@@ -1,3 +1,6 @@
 #include <stdio.h>
 
+#include "polish_notation.h"
+#include "stack.h"
+
 int main() { return 0; }
--- src/polish_notation.c
+++ src/polish_notation.c
@@ -1,1 +1,5 @@
+#include "polish_notation.h"
+
 #include <stdio.h>
+
+#include "stack.h"
