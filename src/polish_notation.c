099e82e7775b8b69f686583f36ef45457122906e
kamil116rus <kh8@rambler.ru>
2023-04-11 22:50:18 +0300

    digit

--- src/polish_notation.c
+++ src/polish_notation.c
@@ -2,9 +2,31 @@
 
 int reverse_polish_notation(char *str) {
     printf("Введите  выражение:\n");
-    int res = SUCCES;
+    int res = SUCCES;       // валидность выражения
     int count_staples = 0;  // счетчик скобок
-    int i = 0;
+    int i = 0, count = 0;
+    char *temp = calloc(MAX_STR, sizeof(char));
+
     while (str[i] != '\n' || str[i] != '\0') {
-        if (str[i] =) }
+        if (is_digit(str[i])) {
+            while (is_digit(str[i])) {
+                temp[count] = str[i];
+                i++;
+                count++;
+            }
+            temp[count] = ' ';
+            count++;
+        }
+        if (str[i] == 'x' || str[i] == 'X') {
+            temp[count] = str[i];
+        }
+    }
+}
+
+int is_digit(char c) {
+    int res = NO;
+    if (c >= 48 && c <= 57) {
+        res = YES;
+    }
+    return res;
 }
