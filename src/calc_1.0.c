506711918e6cf3d2c102697529063ed992079dba
kamil116rus <kh8@rambler.ru>
2023-05-16 12:42:25 +0300

    make commit

--- src/calc_1.0.c
+++ src/calc_1.0.c
@@ -11,11 +11,8 @@
 int main() {
     list *p = NULL;
 
-    // // push_the_list(&p, "cos", NUMBER);
-    // push_the_list(&p, "13", NUMBER);
-    // push_the_list(&p, "sin", NUMBER);
+   
     parser(&p);
     print_list(p);
-    // free(str);
     return 0;
 }
--- src/list.c
+++ src/list.c
@@ -9,21 +9,20 @@ void init(list **top, char *str, type_t type) {
 }
 
 void push_the_list(list **top, char *str, type_t type) {
-    printf("dsfg");
     if (*top == NULL) {
-        printf("top ==NULL\n");
+        // printf("top ==NULL\n");
         list *temp = NULL;
         init(&temp, str, type);
         *top = temp;
-        printf("--%s\n", (*top)->leks);
+        //  printf("--%s\n", (*top)->leks);
     } else if ((*top)->next != NULL) {
-        printf("Идем в конец списка)\n");
+        // printf("Идем в конец списка)\n");
         push_the_list(&(*top)->next, str, type);  //идем в конец списка
     } else {
-        printf("top != NULL\n");
+        //  printf("top != NULL\n");
         list *temp = NULL;
         init(&temp, str, type);
-        printf("-%s\n", str);
+        //  printf("-%s\n", str);
         if (temp != NULL) {
             (*top)->next = temp;
         }
